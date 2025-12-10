71329fa5e8d1136ad799d2445dcd11c3f75e46cb
Dimitrios Lahanas <dimi@Dimitrioss-MacBook-Pro.local>
2025-12-10 15:21:39 +1100

    refactor(LDTheme): change from colorseed to alternative

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -2,18 +2,30 @@ import 'package:flutter/material.dart';
 
 class LDTheme {
   static ThemeData lightTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(
+    colorScheme: ColorScheme(
       brightness: Brightness.light,
-      seedColor: Colors.blue,
+      primary: Colors.black,
+      onPrimary: Colors.white,
+      secondary: Color(0xFF181818),
+      onSecondary: Colors.white,
+      error: Colors.blue,
+      onError: Colors.red,
       surface: Colors.white,
+      onSurface: Colors.black,
     ),
   );
 
   static ThemeData darkTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(
+    colorScheme: ColorScheme(
       brightness: Brightness.dark,
-      seedColor: Colors.blue,
-      surface: Colors.black,
+      primary: Colors.white,
+      onPrimary: Colors.black,
+      secondary: Color(0xFFE5E5E5),
+      onSecondary: Colors.black,
+      error: Colors.red,
+      onError: Colors.blue,
+      surface: Colors.black, 
+      onSurface: Colors.white,
     ),
   );
 }
